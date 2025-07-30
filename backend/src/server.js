4c7b91d51b923e7a9b5d228dbdf0a5229415a96e
Pubudu Wijesundara <neo17641@gmail.com>
2025-07-30 22:22:39 +0530

    Initialize backend, create basic url end points, connect MONGODB and manage environment variables with dotenv

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import noteRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5001
+connectDB()
+
+app.use('/api/notes', noteRoutes)
+
+app.listen(PORT, () => {
+  console.log('Server started on Port:', PORT)
+})
