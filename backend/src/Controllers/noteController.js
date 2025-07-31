c37793b167730691fd310f61145ffed4ced05ff0
Pubudu Wijesundara <neo17641@gmail.com>
2025-07-31 13:29:43 +0530

    Add the newest notes first sorting

--- backend/src/Controllers/noteController.js
+++ backend/src/Controllers/noteController.js
@@ -2,7 +2,7 @@ import Note from '../models/Note.js'
 
 export async function getAllNotes(req, res) {
   try {
-    const notes = await Note.find()
+    const notes = await Note.find().sort({createdAt: -1}) //Show the newest notes first
     res.status(200).json(notes)
   } catch (error) {
     console.error('Error in getAllNotes controller', error)
