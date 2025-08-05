ab629797918a3d225a1021ca8d373cea2f5760b2
Pubudu Wijesundara <neo17641@gmail.com>
2025-08-05 12:15:24 +0530

    Add ors middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
 import noteRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
-import dotenv from 'dotenv'
 import rateLimiter from './middleware/rateLimiter.js'
 
 dotenv.config()
@@ -12,6 +14,9 @@ const PORT = process.env.PORT || 5001
 //middleware
 app.use(express.json())
 app.use(rateLimiter)
+app.use(cors({
+  origin:"http://localhost:5173",
+}))
 
 app.use('/api/notes', noteRoutes)
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
+import axios from 'axios'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [loading, setLoadnig] = useState(true)
 
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:5001/api/notes')
+        console.log(res.data)
+      } catch (error) {
+        console.log('Error fetching notes')
+      }
+    }
+    fetchNotes()
+  }, [])
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
 
       {isRateLimited && <RateLimitedUI />}
