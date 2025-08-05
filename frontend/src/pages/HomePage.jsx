01f573432e88dbc7e22581f8056cdf8faccd4ab2
Pubudu Wijesundara <neo17641@gmail.com>
2025-08-05 11:54:29 +0530

    create RatelimitedUI component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false)
+
   return (
     <div className='min-h-screen'>
       <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   )
 }
