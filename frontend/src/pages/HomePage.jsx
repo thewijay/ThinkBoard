96aac07aef84aedc84b5f94f95574e540c7b374a
Pubudu Wijesundara <neo17641@gmail.com>
2025-08-05 11:41:48 +0530

    create Navbar component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Navbar />
+    </div>
   )
 }
 
